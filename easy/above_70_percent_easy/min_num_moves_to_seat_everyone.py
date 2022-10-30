77fda52fb838506cc4a59c6976559ace83edd432
SamanehGhafouri <samaneh2010gh@gmail.com>
2022-10-30 15:37:51 -0400

    Minimum number of moves to seat everyone

---
+++ easy/above_70_percent_easy/min_num_moves_to_seat_everyone.py
@@ -0,0 +1,29 @@
+"""
+There are n seats and n students in a room. You are given an array seats of length n,
+where seats[i] is the position of the ith seat. You are also given the array students of length n,
+where students[j] is the position of the jth student.
+
+You may perform the following move any number of times:
+
+Increase or decrease the position of the ith student by 1 (i.e., moving the ith student from position x to x + 1 or x - 1)
+Return the minimum number of moves required to move each student to a seat such that no two students are in the same seat.
+
+Note that there may be multiple seats or students in the same position at the beginning.
+"""
+from typing import List
+
+
+def min_moves_to_seat(seats: List[int], students: List[int]) -> int:
+    seats.sort()
+    students.sort()
+    min_number_of_moves = 0
+    for student, seat in zip(students, seats):
+        diff = abs(student - seat)
+        min_number_of_moves += diff
+    return min_number_of_moves
+
+
+if __name__ == '__main__':
+    seats = [3, 1, 5]
+    students = [2, 7, 4]
+    print(min_moves_to_seat(seats, students))
