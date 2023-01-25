4a812c6357068758a2d6c22edafebf55b7e679a7
SamanehGhafouri <samaneh2010gh@gmail.com>
2023-01-24 23:07:09 -0500

    Largest Positive Integer That Exists With Its Negative

---
+++ easy/50_to_70_percent_easy/largest_positive_int_with_its_negative.py
@@ -0,0 +1,34 @@
+"""
+Given an integer array nums that does not contain any zeros, find the largest positive integer k such that
+-k also exists in the array.
+
+Return the positive integer k. If there is no such integer, return -1.
+"""
+from typing import List
+
+
+def find_max_k(nums: List[int]) -> int:
+    set_positive = set()
+    set_negative = set()
+    for num in nums:
+        if num > 0:
+            set_positive.add(num)
+        if num < 0:
+            set_negative.add(num)
+    if len(set_positive) == 0 or len(set_negative) == 0:
+        return - 1
+    for num in range(len(set_positive)):
+        max_num = max(set_positive)
+        negative_num = -abs(max_num)
+        if negative_num in set_negative:
+            return max_num
+        else:
+            set_positive.remove(max_num)
+        if len(set_positive) == 0:
+            return -1
+    return max_num
+
+
+if __name__ == '__main__':
+    nums = [-1, 2, -3, 3]
+    print(find_max_k(nums))
